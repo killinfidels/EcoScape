ca524a6d1d612b3732a2816f59457b7b5afe4ca9
killinfidels <mahdi0800@gmail.com>
2021-07-26 21:11:27 +0200

    First Test

---
+++ EcoScape/src/Test.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+#include <stdio.h>
+
+
+namespace EcoScape
+{
+	void Print()
+	{
+		printf("Welcome to EcoScape Engine!\n");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+
+namespace EcoScape {
+	__declspec(dllimport) void Print();
+}
+
+void main()
+{
+	EcoScape::Print();
+}
